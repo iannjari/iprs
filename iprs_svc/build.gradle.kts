ada0f7eae12db3889e7fe71cb2408876f33213d0
Ian Njari <iannjari@gmail.com>
2024-02-05 14:35:22 +0300

    configure jacoco and sonar for static analysis

--- iprs_svc/build.gradle.kts
+++ iprs_svc/build.gradle.kts
@@ -20,6 +20,6 @@ sonarqube {
         property("sonar.projectKey", "iprs")
         property("sonar.host.url", "https://sonarcloud.io")
         property("sonar.organization", "iannjari")
-//        property("sonar.token", "f86a1089f679685d121004a587a32db2bd8e999f")
+//        property("sonar.token", "")
     }
 }
